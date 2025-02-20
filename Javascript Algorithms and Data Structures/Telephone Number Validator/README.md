b4dc0d41b09ff43cee2abed08b2151089491e5ec
Raqib <nickn.abdullah@gmail.com>
2025-02-20 12:06:05 +0600

    Completed Javascript project -> Telephone Number Validator

---
+++ Javascript Algorithms and Data Structures/Telephone Number Validator/README.md
@@ -0,0 +1,35 @@
+# Telephone Number Validator
+
+This project is a simple web application that validates US telephone numbers. It checks if the input matches any of the valid US phone number formats.
+
+Details in -> https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/build-a-telephone-number-validator-project/build-a-telephone-number-validator.
+
+## Features
+
+- Input field for entering phone numbers
+- Button to check the validity of the phone number
+- Button to clear the input and results
+- Displays whether the phone number is valid or invalid
+
+## Valid Formats
+
+- 1 555-555-5555
+- 1 (555) 555-5555
+- 1(555)555-5555
+- 1 555 555 5555
+- 5555555555
+- 555-555-5555
+- (555)555-5555
+
+The area code is required. Also, if the country code is provided, it must be 1.
+
+## How to Use
+
+1. Enter a phone number in the input field.
+2. Click the "Check Phone Number" button to validate the number.
+3. The result will be displayed below the buttons.
+4. Click the "Clear All" button to clear the input and results.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ Javascript Algorithms and Data Structures/Telephone Number Validator/script.js
@@ -0,0 +1,86 @@
+// get all elements from the form
+const userInput = document.getElementById('user-input');
+const checkBtn = document.getElementById('check-btn');
+const clearBtn = document.getElementById('clear-btn');
+const resultsDiv = document.getElementById('results-div');
+
+/* pattern which matches the following number formats:  
+- 1 555-555-5555
+- 1 (555) 555-5555
+- 1(555)555-5555
+- 1 555 555 5555
+- 5555555555
+- 555-555-5555
+- (555)555-5555
+*/
+const pattern = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+
+// function to check if the input is a valid US number
+function checkValidPhoneNumber(phoneNumber) {
+
+    // removing any non-numeric characters from the input
+    // to get a clean number for additional checks
+    const clean = phoneNumber.replace(/[^0-9]/g, '');
+
+    // clean string must be number, otherwise invalid
+    if (isNaN(clean)) {
+        return false;
+    }
+
+    // cleaned number must be 10 or 11 digits long, otherwise invalid
+    if (clean.length !== 10 && clean.length !== 11) {
+        return false;
+    }
+
+    // if clean number is 11 digits
+    // it must have leading country code '1', otherwise invalid
+    if (clean.length === 11 && clean[0] !== '1') {
+        return false;
+    }
+
+    // finally check pattern match
+    return pattern.test(phoneNumber);
+}
+
+// define a function to append a message to existing resultsDiv
+// taking param of valid or invalid phone number
+function showResult(isEmpty, phoneNumber, isValidNumber) {
+    if (isEmpty){
+        resultsDiv.textContent = 'Please provide a phone number!';
+        resultsDiv.className = 'invalid';
+        return;
+    }
+
+    resultsDiv.textContent = isValidNumber
+        ? `Valid US number: ${phoneNumber}`
+        : `Invalid US number: ${phoneNumber}`;
+
+        resultsDiv.className = isValidNumber ? 'valid' : 'invalid';
+
+}
+
+// add an event listener for when the user clicks the button
+checkBtn.addEventListener('click', function () {
+
+    // get the input value
+    const phoneNumber = userInput.value;
+
+    // check if the input is empty, then show alert, no need to check further
+    if (phoneNumber === '') {
+        // append the error message in the output element
+        showResult(true, null, null)
+        alert('Please provide a phone number');
+        return;
+    }
+
+    const isValidNumber = checkValidPhoneNumber(phoneNumber);
+
+    // append the error message in the output element
+    showResult(phoneNumber, isValidNumber)
+});
+
+// add an event listener for when the user clicks the button
+clearBtn.addEventListener('click', function () {
+    userInput.value = '';
+    resultsDiv.textContent = '';
+});
