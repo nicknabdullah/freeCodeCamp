640f7584de1f3d5d8293c54d0eb17fa275c3477a
Raqib <nickn.abdullah@gmail.com>
2025-04-18 19:32:36 +0600

    Cash Register App -> change 'cid' to original values

--- Javascript Algorithms and Data Structures/Cash Register/script.js
+++ Javascript Algorithms and Data Structures/Cash Register/script.js
@@ -3,15 +3,15 @@ const price = 19.5;
 
 // Cash-in-drawer (cid): available denominations and their total amount
 let cid = [
-    ["PENNY", 0.5],
-    ["NICKEL", 0],
-    ["DIME", 0],
-    ["QUARTER", 0],
-    ["ONE", 0],
-    ["FIVE", 0],
-    ["TEN", 0],
-    ["TWENTY", 0],
-    ["ONE HUNDRED", 0]
+    ['PENNY', 1.01],
+    ['NICKEL', 2.05],
+    ['DIME', 3.1],
+    ['QUARTER', 4.25],
+    ['ONE', 90],
+    ['FIVE', 55],
+    ['TEN', 20],
+    ['TWENTY', 60],
+    ['ONE HUNDRED', 100]
 ];
 
 // Value of each denomination
